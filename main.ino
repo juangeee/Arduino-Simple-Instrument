f2acc4a859290708ba5e805361cc6b75b3370b19
Maishi <lemaishi@gmail.com>
2022-10-07 17:15:32 -0400

    V.2

--- main.ino
+++ main.ino
@@ -1,36 +1,30 @@
-int buzzer=2;
-int pot=A0;
-int notes[14] = {262,294,330,349,392,440,494,523,587,659,698,784,880,988};
-int maxV = 1023;
+int frequency_output=12;
+int voltage_input=A0;
+int notes[15] = {262,294,330,349,392,440,494,523,587,659,698,784,880,988,1047};
+int maximum_voltage = 1023;
+
+int frequencyFinder(int voltage, int maximum_voltage) {
+  int voltage_per_note = maximum_voltage / 15, voltage_checker = voltage_per_note;
+  for (int i = 0; i < 15; i++) {
+    if(voltage <= voltage_checker)
+    {
+      return(notes[i]);
+    }else if (voltage > voltage_per_note * 15)
+    {
+      return(notes[14]);
+    }
+    voltage_checker = voltage_checker + voltage_per_note;
+  };
+}
 
 void setup() {
-  pinMode(buzzer, OUTPUT);
-  pinMode(pot,INPUT);
-  Serial.begin(9600);
+  pinMode(frequency_output, OUTPUT);
+  pinMode(voltage_input,INPUT);
 }
 
 void loop() {
-  int volts=analogRead(pot);
-  int vPerN = maxV / 14;
-  int expPer = vPerN;
-  int counter = 0;
-  boolean cond = true;
-  while (cond)
-  {
-    if(volts <= expPer)
-    {
-      cond = false;
-      int noteFrec = notes[counter];
-      tone(buzzer, noteFrec,150);
-      Serial.println(notes[counter]);
-    }else if (volts > vPerN * 14)
-    {
-      cond = false;
-      tone(buzzer, notes[13], 150);
-      Serial.println(notes[13]);
-    }
-    expPer = expPer + vPerN;
-    counter++;
-  };
+  int v = analogRead(voltage_input);
+  int f = frequencyFinder(v, 1030 );
+  tone(frequency_output, f, 150);
   delay(10);
 }
